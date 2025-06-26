f787eb6963b2055493afe8c2ae6028259b7720d7
Kyle VandenLangenberg <kyle@cbcberry.com>
2025-06-26 00:13:13 -0700

    Fix TypeScript unused variable error in CultivarFilterPanel

--- components/CultivarFilterPanel.tsx
+++ components/CultivarFilterPanel.tsx
@@ -52,7 +52,7 @@ export default function CultivarFilterPanel({ filters, onFiltersChange }: Cultiv
     });
   };
 
-  const getAttributeIcon = (attribute: string) => {
+  const getAttributeIcon = (_attribute: string) => {
     // Icons removed for cleaner UI
     return '';
   };
